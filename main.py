20141a3eb5cb69c3fc9b82a226b3f4e1b163da4f
AlexArutiunian <arutunanalexandr17@gmail.com>
2024-02-04 11:14:28 +0300

    add new way to scrape trough prepared by selenium

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-with open("data.json", "r", encoding="utf-8") as f:
+file_in = "merge_res.json"
+
+with open(file_in, "r", encoding="utf-8") as f:
     datas = json.load(f)
 
 names = []
@@ -14,13 +16,13 @@ for data in datas:
     len_= len(datas)
     print(f"{count}/{len_}")
     
-    if(data.get("wiki")):
+    if(data.get("wiki") != ""):
       print("IT is processed")
       continue  
     name = data["page"]
     name = name.replace(" ", "_")
     names.append(name)
-    req = requests.get(wiki + name)
+    req = requests.get(data["wiki_link"])
     soup = BeautifulSoup(req.text, "html.parser")
     test = soup.find("div", class_="mw-content-container").get_text()
      
@@ -28,9 +30,7 @@ for data in datas:
       print("DOES not exist")
       continue
     
-    print(wiki + name)
-    trs = soup.find_all("tr")
-   # print(trs)
+    print(data["wiki_link"])
    
     divs = soup.find_all("div", class_="mw-body-content")
     bio = str()
@@ -38,7 +38,7 @@ for data in datas:
       bio += d.get_text()
     data["wiki"] = bio
     print(data)
-    with open("data.json", "w", encoding="utf-8") as f:
+    with open("data_.json", "w", encoding="utf-8") as f:
         json.dump(datas, f, indent=2)
 
 
