0cc59746be41ae31d7d1c6a48aa4dc82de15e7ff
AlexArutiunian <arutunanalexandr17@gmail.com>
2024-02-04 11:33:47 +0300

    fix bads tabs

--- main.py
+++ main.py
@@ -19,34 +19,30 @@ for data in datas:
     if(data.get("wiki") != ""):
       print("IT is processed")
       continue  
-    name = data["page"]
-    name = name.replace(" ", "_")
+    name_ = data["page"]
+    name = name_.replace(" ", "_")
     names.append(name)
     try:
-	    req = requests.get(data["wiki_link"])
-	    soup = BeautifulSoup(req.text, "html.parser")
-	    test = soup.find("div", class_="mw-content-container").get_text()
-	     
-	    if("does not have an article") in test:
-			print("DOES not exist")
-			continue
-	    
-	    print(data["wiki_link"])
-	   
-	    divs = soup.find_all("div", class_="mw-body-content")
-	    bio = str()
-	    for d in divs:
-      		bio += d.get_text()
-	    data["wiki"] = bio
-	    print(data)
-	    with open("data_.json", "w", encoding="utf-8") as f:
-			json.dump(datas, f, indent=2)
+      req = requests.get(data["wiki_link"])
+      soup = BeautifulSoup(req.text, "html.parser")
+      test = soup.find("div", class_="mw-content-container").get_text()
+
+      if("does not have an article") in test:
+          print("DOES not exist")
+          continue
+      print(data["wiki_link"])
+
+      divs = soup.find_all("div", class_="mw-body-content")
+      bio = str()
+      for d in divs:
+          bio += d.get_text()
+      data["wiki"] = bio
+      print(data)
+      with open("data_.json", "w", encoding="utf-8") as f:
+          json.dump(datas, f, indent=2)
     except Exception as e:
-        print(f"{e} with {data["page"]}")
-        with("log.txt", "w", encoding="utf-8") as log_file:
-            log_file.write(f"{e} with {data["page"]}")
+        print(f"{e} with {name_}")
+        with open("log.txt", "w", encoding="utf-8") as log_file:
+            log_file.write(f"{e} with {name_}")
         continue    
       		
-
-
-
