49173fa5e5456ce9ea47ac88ad89231970d5304e
AlexArutiunian <arutunanalexandr17@gmail.com>
2024-02-04 11:26:12 -0600

    will fix tabs bad

--- main.py
+++ main.py
@@ -22,24 +22,31 @@ for data in datas:
     name = data["page"]
     name = name.replace(" ", "_")
     names.append(name)
-    req = requests.get(data["wiki_link"])
-    soup = BeautifulSoup(req.text, "html.parser")
-    test = soup.find("div", class_="mw-content-container").get_text()
-     
-    if("does not have an article") in test:
-      print("DOES not exist")
-      continue
-    
-    print(data["wiki_link"])
-   
-    divs = soup.find_all("div", class_="mw-body-content")
-    bio = str()
-    for d in divs:
-      bio += d.get_text()
-    data["wiki"] = bio
-    print(data)
-    with open("data_.json", "w", encoding="utf-8") as f:
-        json.dump(datas, f, indent=2)
+    try:
+	    req = requests.get(data["wiki_link"])
+	    soup = BeautifulSoup(req.text, "html.parser")
+	    test = soup.find("div", class_="mw-content-container").get_text()
+	     
+	    if("does not have an article") in test:
+			print("DOES not exist")
+			continue
+	    
+	    print(data["wiki_link"])
+	   
+	    divs = soup.find_all("div", class_="mw-body-content")
+	    bio = str()
+	    for d in divs:
+      		bio += d.get_text()
+	    data["wiki"] = bio
+	    print(data)
+	    with open("data_.json", "w", encoding="utf-8") as f:
+			json.dump(datas, f, indent=2)
+    except Exception as e:
+        print(f"{e} with {data["page"]}")
+        with("log.txt", "w", encoding="utf-8") as log_file:
+            log_file.write(f"{e} with {data["page"]}")
+        continue    
+      		
 
 
 
